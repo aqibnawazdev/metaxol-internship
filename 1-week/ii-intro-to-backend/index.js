655a7c104c59cba65682ac0f318a66fcf4b80605
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-01-16 18:15:40 +0500

    create basic node server

--- 1-week/ii-intro-to-backend/index.js
+++ 1-week/ii-intro-to-backend/index.js
@@ -1,1 +1,8 @@
-console.log("Hello world")
+const http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {"Content-type": "text/plain"})
+
+    res.end("Hello, world server created...")
+}).listen(8081)
